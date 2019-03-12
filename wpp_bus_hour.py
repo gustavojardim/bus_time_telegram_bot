021b23e0ec42b3e8a801bd187abbd0ded6e26cc7
Gustavo Jardim <gustavo.spadotto@gmail.com>
2019-03-12 14:19:40 -0300

    Script is working but now it uses Telegram Bot to send messages!

--- wpp_bus_hour.py
+++ wpp_bus_hour.py
@@ -1,26 +1,40 @@
 from lxml import html
 from bs4  import BeautifulSoup
 
-import re
+import re 		as regex
 import urllib2  as url2
 import datetime as date
+import telegram
+
+def find_next_leave_time(all_leave_times):
+	for hour in all_leave_times:
+		if hour >= current_hh_mm:
+			print hour
+			return hour
+
+def send(msg, chat_id, token):
+	bot = telegram.Bot(token=token)
+	bot.sendMessage(chat_id=chat_id, text=msg)
+			
+my_token = '748715175:AAE7a1JQ-Hga5y2utDlyk0kXMuqS7QCAgHY'
+
+result = None
+
+regexHourMinute = '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
+
+url = 'http://www.realrodovias.com.br/?pagina=itinerariosPesquisa&detalhe=&linha=COMUNIDADE+02&tabela=horario&enviaConsulta=Consultar'
 
 current_time   = date.datetime.now()
 current_hour   = current_time.hour
 current_minute = current_time.minute
 current_hh_mm  = str(current_hour) + ":" + str(current_minute)
 
-url = 'http://www.realrodovias.com.br/?pagina=itinerariosPesquisa&detalhe=&linha=COMUNIDADE+02&tabela=horario&enviaConsulta=Consultar'
-
-regexHourMinute = '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
-
 html = url2.urlopen(url).read()
 
 soup = BeautifulSoup(html, features = "lxml")
 
-test = soup.find_all(":")
+all_leave_times = soup(text=regex.compile(regexHourMinute))
 
-for hour in soup(text=re.compile(regexHourMinute)):
-    if hour >= current_hh_mm:
-		print hour
-		break
+next_leave_time = find_next_leave_time(all_leave_times)
+
+send(next_leave_time, 155656277, my_token) 
