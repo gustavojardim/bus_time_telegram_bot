a72111e939ce60ad8d98a93ac7cef0bb0f0624b0
Gustavo Jardim <gustavo.spadotto@gmail.com>
2019-03-15 19:39:51 -0300

    Now the script reads the environment variables to get the chat id and the bot's token

--- wpp_bus_hour.py
+++ wpp_bus_hour.py
@@ -1,27 +1,29 @@
-from lxml import html
-from bs4  import BeautifulSoup
+from bs4  				import BeautifulSoup
+from lxml 				import html
+from boto.s3.connection import S3Connection
 
+import os
 import re 		as regex
-import urllib2  as url2
+import urllib3  as url3
 import datetime as date
 import telegram
+
+REGEX_HOUR_MINUTE = '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
 
 def find_next_leave_time(all_leave_times):
 	for hour in all_leave_times:
 		if hour >= current_hh_mm:
-			print hour
+			print (hour)
 			return hour
 
 def send(msg, chat_id, token):
 	bot = telegram.Bot(token=token)
 	bot.sendMessage(chat_id=chat_id, text=msg)
 			
-my_token   = 'telegram bot token'
-my_chat_id = 'telegram chat id'
+my_token   = os.getenv('TELEGRAM_BOT_TOKEN')
+my_chat_id = os.getenv('CHAT_ID')
 
 result = None
-
-regexHourMinute = '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
 
 url = 'http://www.realrodovias.com.br/?pagina=itinerariosPesquisa&detalhe=&linha=COMUNIDADE+02&tabela=horario&enviaConsulta=Consultar'
 
@@ -30,11 +32,13 @@ current_hour   = current_time.hour
 current_minute = current_time.minute
 current_hh_mm  = str(current_hour) + ":" + str(current_minute)
 
-html = url2.urlopen(url).read()
+http = url3.PoolManager()
 
-soup = BeautifulSoup(html, features = "lxml")
+response = http.request('GET', url)
 
-all_leave_times = soup(text=regex.compile(regexHourMinute))
+soup = BeautifulSoup(response.data, features = "lxml")
+
+all_leave_times = soup(text=regex.compile(REGEX_HOUR_MINUTE))
 
 next_leave_time = find_next_leave_time(all_leave_times)
 
